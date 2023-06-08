03e38c899944d8922dc15f03fc57eadf514fb3b2
Lowkey <i202420@nu.edu.pk>
2023-06-08 13:56:54 +0500

    updated

--- utils/supabase.js
+++ utils/supabase.js
@@ -5,3 +5,6 @@ const supabaseUrl = 'https://uxyunknjydijxakiexzw.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV4eXVua25qeWRpanhha2lleHp3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzY0NDE5ODgsImV4cCI6MTk5MjAxNzk4OH0.9Vz-cx5XQmjNcBVuIk3kQ8xObosHYo8yOG0Je7C-kyM'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
+
+
+const baseURL = "http://localhost:3001"
